98246b4a694e2336bcf0f85d62bc609d00cbe9f4
Navin136 <nkwhitehat@gmail.com>
2022-06-29 22:31:56 +0530

    Merge setup and extract script as unpack_rom
    * Move sdat2img, img2sdat, make_ext4fs to tools directory
    
    Add empty repack_rom script (will be added soon)

--- unpack_rom.sh
+++ unpack_rom.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+
+W_DIR=($(pwd))
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+DRED='\033[1;31m'
+DGREEN='\033[1;32m'
+DORANGE='\033[1;33m'
+DBLUE='\033[1;34m'
+DPURPLE='\033[1;35m'
+DCYAN='\033[1;36m'
+
+
+if [ -f "/usr/bin/apt-get" ]; then
+DISTRO="debian"
+printf "${DGREEN} Your machine is installed with Debian/Ubuntu Linux\n"
+fi
+if [ -f "/usr/bin/pacman" ]; then
+DISTRO="arch"
+printf "${DGREEN} Your machine is installed with Arch Linux\n"
+fi
+printf "${ORANGE}"
+if [ $DISTRO == debian ];then
+	sudo apt-get update && sudo apt-get install git brotli python cmake zip unzip figlet rar unrar -y
+else
+	sudo pacman -Syy && sudo pacman -S android-tools cmake git python brotli zip unzip rar figlet unrar --noconfirm
+fi
+clear
+
 printf "	${ORANGE}What's Your Rom zip location\n"
 printf "	${ORANGE}For Example: /home/navin/stock/UL-ASUS.zip\n"
 read -p "Enter Here: " ZIPLOC
@@ -29,8 +62,8 @@ printf "${DRED} Converting sdat to img\n"
 
 printf "${DORANGE} Wait for 5 Minutes\n"
 
-./../sdat2img.py system.transfer.list system.new.dat system.img > /dev/null 2>&1
-./../sdat2img.py vendor.transfer.list vendor.new.dat vendor.img > /dev/null 2>&1
+./../tools/sdat2img.py system.transfer.list system.new.dat system.img > /dev/null 2>&1
+./../tools/sdat2img.py vendor.transfer.list vendor.new.dat vendor.img > /dev/null 2>&1
 rm vendor.transfer.list
 rm system.transfer.list
 rm system.new.dat
